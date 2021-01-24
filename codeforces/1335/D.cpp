c2eec8cf992e117e8f7d0a95f926b8793824dbc0
Abhas Kumar <abhas113dba@gmail.com>
2021-01-24 17:16:00 +0530

    Add solution for problem `D - Anti-Sudoku` Link: https://codeforces.com/contest/1335/problem/D Tags: constructive algorithms, implementation, *1300 Ref: https://codeforces.com/contest/1335/submission/105220426

---
+++ codeforces/1335/D.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long 
+#define deb(x) cout<<#x<<" "<<x<<endl
+#define fo(i,n) for(int i=0;i<n;i++)
+#define nl cout<<endl;
+#define inputi(a) scanf("%d",&a)
+#define inputl(a) scanf("%lld",&a)
+#define printi(a) printf("%d ",a)
+#define printl(a) printf("%lld ",a)
+#define vi vector<int>
+#define vl vector<ll>
+const ll Max2=1e9+7;
+const int Max1=1e6+1;
+ 
+template<typename... T>
+void read(T&&... arg){
+	((cin>>arg),...);
+}
+ 
+template<typename... T> 
+void write(T&&... arg){ //rvalue reference (new to cpp)
+	((cout<<arg<<" "),...);
+	//cout<<endl;
+}
+
+void self_max(ll &a, ll b){
+	a=max(a,b);
+}
+
+void self_min(ll &a, ll b){
+	a=min(a,b);
+}
+
+void solve(){
+	string s;
+	for (int i = 0; i < 9; ++i) {
+		cin >> s;
+		for(int j=0;j<9;++j){
+			if(s[j]=='8') s[j]='7';
+		}
+		cout<<s<<'\n';
+	}
+}
+
+void solve_t(){
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+}
+
+int main(){
+
+	solve_t();
+}
