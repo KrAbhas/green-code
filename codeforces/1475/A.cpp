fb651d6da0f95a03e435167e14608cac8497abcc
Abhas Kumar <abhas113dba@gmail.com>
2021-01-25 21:06:00 +0530

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105355774

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long 
+#define deb(x) cout<<#x<<" "<<x<<endl
+#define fo(i,n) for(int i=0;i<n;i++)
+#define nl cout<<endl;
+#define inputi(a) scanf("%d",&a)
+#define inputl(a) scanf("%lld",&a)
+#define printi(a) printf("%d ",a)
+#define printl(a) printf("%lld ",a)
+#define vi vector<int>
+#define vl vector<ll>
+const ll Max2=1e9+7;
+const int Max1=1e6+1;
+ 
+template<typename... T>
+void read(T&&... arg){
+	((cin>>arg),...);
+}
+ 
+template<typename... T> 
+void write(T&&... arg){ //rvalue reference (new to cpp)
+	((cout<<arg<<" "),...);
+	//cout<<endl;
+}
+
+void self_max(ll &a, ll b){
+	a=max(a,b);
+}
+
+void self_min(ll &a, ll b){
+	a=min(a,b);
+}
+
+void solve(){
+	ll n;
+	cin>>n;
+	while(n%2==0 && n!=0){
+		n=n/2;
+	}
+	if(n==0 || n==1){
+		cout<<"NO\n";
+	}
+	else{
+		cout<<"YES\n";
+	}
+}
+
+void solve_t(){
+	int t;
+	cin>>t;
+	while(t--){
+		//deb(t);
+		solve();
+		//deb(t);
+	}
+}
+
+int main(){
+
+	solve_t();
+}
