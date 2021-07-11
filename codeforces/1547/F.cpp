9f5b2cff64392ebcc13674f21fcf2162ae859696
Abhas Kumar <abhas113dba@gmail.com>
2021-07-11 23:44:00 +0530

    Add solution for problem `F - Array Stabilization (GCD version)` Link: https://codeforces.com/contest/1547/problem/F Tags: binary search, brute force, data structures, divide and conquer, number theory, two pointers, *1900 Ref: https://codeforces.com/contest/1547/submission/122138198

--- codeforces/1547/F.cpp
+++ codeforces/1547/F.cpp
@@ -3,6 +3,8 @@ using namespace std;
 #define ll long long
 
 int n;
+int arr[200001];
+
 struct segtree{
 	int size;
 	vector<int> gcd;
@@ -80,11 +82,9 @@ bool good(int x, segtree& st, vector<int>& vec){
 }
 
 int main(){
+	
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	
 	int tt;
 	cin>>tt;
 	while(tt--){
