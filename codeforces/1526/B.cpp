9decf1d099dfff70a262a16042f7ecc6b1e9f59d
Abhas Kumar <abhas113dba@gmail.com>
2021-12-11 04:52:00 +0530

    Add solution for problem `B - I Hate 1111` Link: https://codeforces.com/contest/1526/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1526/submission/138687575

---
+++ codeforces/1526/B.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define fact(a,b) factorial (a, b)
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+#define fob(i, a, n) for (ll i = a; i >= n; i--)
+ 
+//_____________________________________________________________________________DSU
+class dsu {
+public:
+	vector<ll> p;
+	ll n;
+	dsu (ll k) : n(k) {
+		p.resize(n);
+		iota(p.begin(), p.end(), 0);
+	}
+ 
+	inline ll get(ll x) {
+		return (x == p[x] ? x : (p[x] = get(p[x])));
+	}
+ 
+	inline bool unite(ll x, ll y) {
+		x = get(x);
+		y = get(y);
+		if (x != y) {
+			p[x] = y;
+			return true;
+		}
+		return false;
+	}
+};
+//---------------------------------------------------------------------------factorial, ncr
+ll factorial (ll k, ll s) {
+	ll f = 1;
+	for (ll i = s; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+ 
+ll ncr(ll n, ll r) {
+	ll rd = n - r;
+	ll m = min(r, rd);
+	ll nf = fact(n, (rd + 1));
+	ll rf = fact((n - m), 1);
+	ll ans = nf * 1.0 / rf;
+	return ans;
+}
+ 
+ll sgn(ll &a, ll &b) {
+	if (b - a > 0) return 1;
+	if (b - a < 0) return -1;
+	return 0;
+}
+ 
+bool my_cmp(const pair<ll,ll> &a, const pair<ll,ll> &b) {
+	return a.second < b.second;
+}
+ 
+//***************************************************************************implement
+int main() {
+	ios::sync_with_stdio(false);
+  	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		ll n;
+		cin >> n;
+		ll b = 1;
+		bool ok = false;
+		while (true) {
+			if (n - b * 111 < 0) {
+				break;
+			}
+			else {
+				if ((n - b * 111) % 11 == 0){
+					ok = true;
+					break;
+				}
+			}
+			b++;
+		}
+		if (n % 11 == 0)
+			ok = true; 
+		cout << (ok? "YES\n" : "NO\n"); 
+	} 
+} 
