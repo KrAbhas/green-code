562490648a2e6377aa84733a5de59c2b630a43f4
Abhas Kumar <abhas113dba@gmail.com>
2021-12-16 16:48:00 +0530

    Add solution for problem `B - Swaps` Link: https://codeforces.com/contest/1573/problem/B Tags: greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1573/submission/139473701

---
+++ codeforces/1573/B.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define fact(a,b) factorial (a, b)
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+#define fob(i, a, n) for (ll i = a; i >= n; i--)
+ 
+//_____________________________________________________________________________DSU
+class dsu {
+public:
+	vector<ll> p;
+	ll n;
+	dsu (ll k) : n(k) {
+		p.resize(n);
+		iota(p.begin(), p.end(), 0);
+	}
+ 
+	inline ll get(ll x) {
+		return (x == p[x] ? x : (p[x] = get(p[x])));
+	}
+ 
+	inline bool unite(ll x, ll y) {
+		x = get(x);
+		y = get(y);
+		if (x != y) {
+			p[x] = y;
+			return true;
+		}
+		return false;
+	}
+};
+//---------------------------------------------------------------------------factorial, ncr
+ll factorial (ll k, ll s) {
+	ll f = 1;
+	for (ll i = s; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+ 
+ll ncr(ll n, ll r) {
+	ll rd = n - r;
+	ll m = min(r, rd);
+	ll nf = fact(n, (rd + 1));
+	ll rf = fact((n - m), 1);
+	ll ans = nf * 1.0 / rf;
+	return ans;
+}
+ 
+ll sgn(ll &a, ll &b) {
+	if (b - a > 0) return 1;
+	if (b - a < 0) return -1;
+	return 0;
+}
+
+struct tri{
+	ll a, b, c;
+};
+typedef struct tri triple; 
+
+bool my_cmp(const triple &a, const triple &b) {
+	if (a.a == b.a)
+		return a.b > b.b;
+	return a.a < b.a;
+}
+ 
+//***************************************************************************implement
+
+ll factor(int k) {
+	ll f = 1;
+	for (int i = 2; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+  	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while(tt --) {
+		int n;
+		cin >> n;
+		pair<int,int> a[n];
+		int b[n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i].first;
+			a[i].second = i;
+		}
+		sort (a, a + n);
+		int k = 0;
+		int m = 9999999;
+		for (int i = 0; i < n; i++) {
+			cin	>> b[i];
+			int p = 999999;
+			if (b[i] < a[k].first) 
+				continue;
+			while (b[i] > a[k].first) {
+				p = min (p, a[k].second);
+				k++;
+			}
+			m = min (m, p + i);
+		}
+		cout << m << endl;
+	}
+} 
