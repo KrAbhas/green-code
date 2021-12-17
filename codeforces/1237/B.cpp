4c1d0858066943cc3d02c63c5e506541bba3bb46
Abhas Kumar <abhas113dba@gmail.com>
2021-12-17 16:09:00 +0530

    Add solution for problem `B - Balanced Tunnel` Link: https://codeforces.com/contest/1237/problem/B Tags: data structures, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1237/submission/139625663

---
+++ codeforces/1237/B.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define fact(a,b) factorial (a, b)
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+#define fob(i, a, n) for (ll i = a; i >= n; i--)
+ 
+//_____________________________________________________________________________DSU
+class dsu {
+public:
+	vector<ll> p;
+	ll n;
+	dsu (ll k) : n(k) {
+		p.resize(n);
+		iota(p.begin(), p.end(), 0);
+	}
+ 
+	inline ll get(ll x) {
+		return (x == p[x] ? x : (p[x] = get(p[x])));
+	}
+ 
+	inline bool unite(ll x, ll y) {
+		x = get(x);
+		y = get(y);
+		if (x != y) {
+			p[x] = y;
+			return true;
+		}
+		return false;
+	}
+};
+//---------------------------------------------------------------------------factorial, ncr
+ll factorial (ll k, ll s) {
+	ll f = 1;
+	for (ll i = s; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+ 
+ll ncr(ll n, ll r) {
+	ll rd = n - r;
+	ll m = min(r, rd);
+	ll nf = fact(n, (rd + 1));
+	ll rf = fact((n - m), 1);
+	ll ans = nf * 1.0 / rf;
+	return ans;
+}
+ 
+ll sgn(ll &a, ll &b) {
+	if (b - a > 0) return 1;
+	if (b - a < 0) return -1;
+	return 0;
+}
+
+struct tri{
+	ll a, b, c;
+};
+typedef struct tri triple; 
+
+bool my_cmp(const triple &a, const triple &b) {
+	if (a.a == b.a)
+		return a.b > b.b;
+	return a.a < b.a;
+}
+ 
+//***************************************************************************implement
+
+ll factor(int k) {
+	ll f = 1;
+	for (int i = 2; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+  	cin.tie(0);
+	int n;
+	cin >> n;
+	int a[n];
+	int b;
+	bool arr[n + 1];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		arr[a[i]] = false;
+	}
+	int k = 0;
+	ll res = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> b;
+		if (arr[b])
+			continue;
+		arr[b] = true;
+		if (b != a[k]){
+			res++;
+			//cout << b << a[k] << " ";
+			continue;
+		}
+		while (k < n && arr[a[k]] == true) {
+			k++;
+		}
+	}
+	cout << res << endl;
+} 
