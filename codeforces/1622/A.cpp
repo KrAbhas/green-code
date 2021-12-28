ebb8091609686213cd417aa8432fc1887014b901
Abhas Kumar <abhas113dba@gmail.com>
2021-12-28 17:25:00 +0530

    Add solution for problem `A - Construct a Rectangle` Link: https://codeforces.com/contest/1622/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1622/submission/140893295

---
+++ codeforces/1622/A.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define fact(a,b) factorial (a, b)
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+#define fob(i, a, n) for (ll i = a; i >= n; i--)
+#define all(x) x.begin(),x.end()
+ 
+//_____________________________________________________________________________DSU
+class dsu {
+public:
+	vector<ll> p;
+	ll n;
+	dsu (ll k) : n(k) {
+		p.resize(n);
+		iota(p.begin(), p.end(), 0);
+	}
+ 
+	inline ll get(ll x) {
+		return (x == p[x] ? x : (p[x] = get(p[x])));
+	}
+ 
+	inline bool unite(ll x, ll y) {
+		x = get(x);
+		y = get(y);
+		if (x != y) {
+			p[x] = y;
+			return true;
+		}
+		return false;
+	}
+};
+//---------------------------------------------------------------------------factorial, ncr
+ll factorial (ll k, ll s) {
+	ll f = 1;
+	for (ll i = s; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+ 
+ll ncr(ll n, ll r) {
+	ll rd = n - r;
+	ll m = min(r, rd);
+	ll nf = fact(n, (rd + 1));
+	ll rf = fact((n - m), 1);
+	ll ans = nf * 1.0 / rf;
+	return ans;
+}
+ 
+ll sgn(ll &a, ll &b) {
+	if (b - a > 0) return 1;
+	if (b - a < 0) return -1;
+	return 0;
+}
+
+struct tri{
+	ll a, b, c;
+};
+typedef struct tri triple; 
+
+bool my_cmp(const triple &a, const triple &b) {
+	if (a.a == b.a)
+		return a.b > b.b;
+	return a.a < b.a;
+}
+ 
+//***************************************************************************implement
+
+ll factor(int k) {
+	ll f = 1;
+	for (int i = 2; i <= k; i++) {
+		f *= i;
+	}
+	return f;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+  	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int arr[3];
+		for (int i = 0; i < 3; i++)
+			cin >> arr[i];
+		sort(arr, arr + 3);
+		if (arr[0] == arr[1]) {
+			if (arr[2] % 2 == 0){
+				cout << "YES\n";
+				continue;
+			}
+			else {
+				cout << "NO\n";
+				continue;
+			}
+		}
+		else if (arr[1] == arr[2]) {
+			if (arr[0] % 2 == 0) {
+				cout << "YES\n";
+			}
+			else cout << "NO\n";
+		}
+		else {
+			if (arr[0] + arr[1] == arr[2])
+				cout << "YES\n";
+			else cout << "NO\n";
+		}
+	}
+} 
