385bdbcb8b7a86649490a654859b2fec1e3b434e
Abhas Kumar <abhas113dba@gmail.com>
2021-12-31 03:49:00 +0530

    Add solution for problem `D - Keep the Average High` Link: https://codeforces.com/contest/1616/problem/D Tags: data structures, dp, greedy, math, *2000 Ref: https://codeforces.com/contest/1616/submission/141201462

--- codeforces/1616/D.cpp
+++ codeforces/1616/D.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 #define ll long long
-#define intl int64_t
 
 int main(){
 
@@ -19,24 +18,22 @@ int main(){
 			cin >> a[i];
 		}
 		cin >> t;
-		intl pref[n + 1];
+		int pref[n + 1];
 		pref[0] = 0;
 		for (ll i = 0; i < n; i++) {
 			a[i] = a[i] - t;
 			pref[i + 1] = pref[i] + a[i];
 		}
-		intl until = -1e18;
-		intl pre = pref[0];
-		int res = 0;
-		for (int i = 0; i < n; i++) {
-			if (pref[i + 1] >= until) {
-				++res;
-				until = max(pre, until);
-				pre = pref[i + 1];
+		int mxpref = pref[0];
+		int res = n;
+		for (int i = 1; i < n; i++) {
+			if ((pref[i + 1] - mxpref) < 0) {
+				--res;
+				mxpref = pref[i + 1];
+				++i;
 			}
 			else {
-				until = -1e18;
-				pre = pref[i + 1];
+				mxpref = max(mxpref, pref[i]);
 			}
 		}
 		cout << res << endl;
