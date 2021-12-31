ea4a19c12ef7b34fcd11e53cbb03ab1c5894e6fe
Abhas Kumar <abhas113dba@gmail.com>
2022-01-01 03:58:00 +0530

    Add solution for problem `B - Build the Permutation` Link: https://codeforces.com/contest/1608/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1608/submission/141277299

--- codeforces/1608/B.cpp
+++ codeforces/1608/B.cpp
@@ -14,33 +14,37 @@ int main(){
 	while (tt--) {
 		ll n, a, b;
 		cin >> n >> a >> b;
-		if (abs(a - b) > 1 || (a + b + 2) > n) {
-			cout << -1 << endl;
-		}
+		if (abs(a - b) > 1 || (a + b + 2) > n)
+			cout << -1;
+		else if ((2 * a >= n || 2 * b >= n))
+			cout << -1;
 		else {
 			int arr[n];
-			for (int i = 0; i < n; i++) 
+			for (int i = 0; i < n; i++) {
 				arr[i] = (i + 1);
-			int p = 0;
+			}
 			if (a == b) {
-				p = 1;
+				int p = 1;
 				while (a--) {
 					swap(arr[p], arr[p + 1]);
-					p += 2;
-				}
-			}
-			else if (a > b) {
-				p = n - 1;
-				while (a--) {
-					swap(arr[p], arr[p - 1]);
-					p -= 2;
+					p = p + 2;
 				}
 			}
 			else {
-				p = 0;
-				while (b--) {
-					swap(arr[p], arr[p + 1]);
-					p += 2;
+				int p;
+				if (a < b) {
+					p = 0;
+					while (b--) {
+						swap(arr[p], arr[p + 1]);
+						p += 2;
+					}
+				}
+				else {
+					p = n - 1;
+					while (a--) {
+						swap(arr[p], arr[p - 1]);
+						p -= 2;
+					}
 				}
 			}
 			for (int i = 0; i < n; i++) {
