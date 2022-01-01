0ea452d613b90e51bb0a5d4c2fa729a6abe8a93c
Abhas Kumar <abhas113dba@gmail.com>
2022-01-01 16:46:00 +0530

    Add solution for problem `C - Product of Three Numbers` Link: https://codeforces.com/contest/1294/problem/C Tags: greedy, math, number theory, *1300 Ref: https://codeforces.com/contest/1294/submission/141307959

--- codeforces/1294/C.cpp
+++ codeforces/1294/C.cpp
@@ -1,76 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-#define ll long long 
-#define deb(x) cout<<#x<<" "<<x<<endl
-#define fo(i,n) for(int i=0;i<n;i++)
-#define nl cout<<endl;
-#define inputi(a) scanf("%d",&a)
-#define inputl(a) scanf("%lld",&a)
-#define printi(a) printf("%d ",a)
-#define printl(a) printf("%lld ",a)
-#define vi vector<int>
-#define vl vector<ll>
-const ll Max2=1e9+7;
-const int Max1=1e6+1;
- 
-template<typename... T>
-void read(T&&... arg){
-	((cin>>arg),...);
-}
- 
-template<typename... T> 
-void write(T&&... arg){ //rvalue reference (new to cpp)
-	((cout<<arg<<" "),...);
-	//cout<<endl;
-}
 
-void self_max(ll &a, ll b){
-	a=max(a,b);
-}
-
-void self_min(ll &a, ll b){
-	a=min(a,b);
-}
-
-void solve(){
-	ll n;
-	cin>>n;
-	int a=2;int b=3;
-	int c=n;
-	for(int i=2;i<n/(i*(i+1));i++){
-		//int k=n/(i*(i+1));
-		for(int j=i+1;true;j++){
-			int k=n/(i*j);
-			if(j>=k){
-				break;
-			}
-			double d=n*1.0/(i*j);
-			//deb(k);
-			//deb(d);
-			if(k==d){
-				c=k;
-				a=i;
-				b=j;
-				cout<<"YES\n";
-				cout<<a<<" "<<b<<" "<<c<<endl;
-				return;
-			}
-			
-		}
-	}
-	cout<<"NO\n";
-}
-
-void solve_t(){
-	int t;
-	cin>>t;
-	while(t--){
-		solve();
-	}
-}
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
 
 int main(){
-
-	solve_t();
+	const int inf = 1e9;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll tt;
+	cin >> tt;
+	while (tt--) {
+		ll n;
+		cin >> n;
+		ll a, b, c;
+		a = 2, b = 2, c = 2;
+		for (int i = 2; n / (i * (i + 1)) > i + 1; i++) {
+			a = i;
+			for (int j = i + 1; (n / (i * j)) > j; j++) {
+				c = n / (i * j);
+				b = j;
+				if (i * j * c == n && c > i && c > j) {
+					break;
+				}	
+			}
+			if (a * b * c == n && c > a && c > b)
+				break;
+		}
+		if (a * b * c == n && c > a && c > b)
+			cout << "YES\n" << a << " " << b << " " << c << endl;
+		else cout << "NO\n";
+	}
 }
