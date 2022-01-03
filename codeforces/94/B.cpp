40693daae51fb95d8f8cac7408f49f820cd0cff4
Abhas Kumar <abhas113dba@gmail.com>
2022-01-04 02:36:00 +0530

    Add solution for problem `B - Friends` Link: https://codeforces.com/contest/94/problem/B Tags: graphs, implementation, math, *1300 Ref: https://codeforces.com/contest/94/submission/141587577

--- codeforces/94/B.cpp
+++ codeforces/94/B.cpp
@@ -9,19 +9,24 @@ int main(){
 	const ll inf = 1e9 + 7;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	int arr[6][6];
 	int n;
 	cin >> n;
+	memset(arr, 0, sizeof(arr));
 	int x, y;
-	vector<vector<int>> v(6);
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
-		v[x].push_back(y);
-		v[y].push_back(x);
+		arr[x][y] = 1;
+		arr[y][x] = 1;
 	}
 	for (int i = 1; i < 6; i++) {
-		if (v[i].size() >= 3 || 4 - v[i].size() >= 3) {
-			cout << "WIN\n";
-			return 0;
+		for (int j = i + 1; j < 6; j++) {
+			for (int k = j + 1; k < 6; k++) {
+				if (arr[i][j] == arr[j][k] && arr[j][k] == arr[k][i] && arr[k][i] == arr[i][j]) {
+					cout << "WIN\n";
+					return 0;
+				}
+			}
 		}
 	}
 	cout << "FAIL\n";
