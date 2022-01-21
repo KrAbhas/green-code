2782008b61eab322eb1eb80c8cb479014a31ed14
Abhas Kumar <abhas113dba@gmail.com>
2022-01-21 06:43:00 +0530

    Add solution for problem `B2 - Books Exchange (hard version)` Link: https://codeforces.com/contest/1249/problem/B2 Tags: dfs and similar, dsu, math, *1300 Ref: https://codeforces.com/contest/1249/submission/143467624

---
+++ codeforces/1249/B2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+
+const int maX = 1e4 + 7;
+
+long long power(long long x, long long y, long long p) 
+{ 
+    long long res;
+    return res; 
+} 
+
+
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n; cin >> n; 
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) cin >> a[i];
+		vector<int> v(n + 1, 0);
+		for (int i = 0; i < n; i++) {
+			if (v[i + 1] == 0) {
+				vector<int> vec;
+				int k = a[i];
+				vec.pb(k);
+				while (k != i + 1) {
+					k = a[k - 1];
+					vec.pb(k);
+				}
+				int t = (int)vec.size();
+				for (int j = 0; j < t; j++) {
+					v[vec[j]] = t;
+				}
+			}
+			cout << v[i + 1] << " ";
+		}
+		cout << endl;
+	}
+}       	
+
