2ba0f172b0dd51837c05fc26b0306d3d8254515c
Abhas Kumar <abhas113dba@gmail.com>
2022-01-27 20:09:00 +0530

    Add solution for problem `A - Min Max Swap` Link: https://codeforces.com/contest/1631/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1631/submission/144171140

---
+++ codeforces/1631/A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+
+const ll maX = 998244353;
+
+long long power(long long x, long long y, long long p) 
+{ 
+    long long res;
+    return res; 
+} 
+
+
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		vector<int> b(n);
+		for (int i = 0; i < n; i++) cin >> a[i];
+		for (int i = 0; i < n; i++) cin >> b[i];
+		for (int i = 0; i < n; i++) {
+			if (a[i] > b[i]) {
+				swap(b[i], a[i]);
+			}
+		}
+		int p, q;
+		p = 0; q = 0;
+		for (int i = 0; i < n; i++) {
+			asnmx(p,a[i]);
+			asnmx(q,b[i]);
+		}
+		cout << p * q << endl;
+	}
+}       	
