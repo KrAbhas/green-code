b983a13915dd45df707ac2a62ac7a107b30fca1c
Abhas Kumar <abhas113dba@gmail.com>
2022-01-30 15:23:00 +0530

    Add solution for problem `C - Palindromifier` Link: https://codeforces.com/contest/1421/problem/C Tags: constructive algorithms, strings, *1400 Ref: https://codeforces.com/contest/1421/submission/144490970

---
+++ codeforces/1421/C.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+
+const ll maX = 998244353;
+
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
+	cin >> s;
+	int n = s.length();
+	printf("3\nR %d\nL %d\nL %d\n", n - 1, n, 2);
+}       	
