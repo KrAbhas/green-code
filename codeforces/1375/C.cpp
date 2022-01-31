8784b0c5a91e76d768f812530d4966eace8d4bcd
Abhas Kumar <abhas113dba@gmail.com>
2022-01-31 10:09:00 +0530

    Add solution for problem `C - Element Extermination` Link: https://codeforces.com/contest/1375/problem/C Tags: constructive algorithms, data structures, greedy, *1400 Ref: https://codeforces.com/contest/1375/submission/144611936

---
+++ codeforces/1375/C.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+
+const ll maX = 998244353;
+
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		int x;
+		cin >> x;
+		int y;
+		for (int i = 0; i < n - 1; i++) cin >> y;
+		if (x < y) cout << "YES\n";
+		else cout << "NO\n";
+	}
+}       	
