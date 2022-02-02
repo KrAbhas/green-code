59228cb0088a2294b833c85e64cb22179cadde18
Abhas Kumar <abhas113dba@gmail.com>
2022-02-02 10:02:00 +0530

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/144897163

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,25 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-#define ll long long
-#define all(x) (x).begin(), (x).end()
-#define intl int64_t
-#define pb(x) push_back(x)
-#define unmap unordered_map
-#define unset unordered_set
-#define bit_set_count(x) __builtin_popcountll(x)
-// use for unordered map (for high collison):
-// um.reserve(4096); um.max_load_factor(0.25);
-template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
-template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
-template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
-template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
-typedef pair<int, int> pi;
-#define rep(i,a,n) for (int i = a; i < n; i++)
-
-#define PI 3.14159265
-
-const int maX = 2 * 1e5 + 1;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef double db;
+mt19937 mrand(random_device{}()); 
+const ll mod=1000000007;
+int rnd(int x) { return mrand() % x;}
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+// head
 
 int _,n,k;
 ll a[1010];
