f97722b8c7b91d9d613ac8bca812e8cfab8702d1
Abhas Kumar <abhas113dba@gmail.com>
2022-02-03 08:37:00 +0530

    Add solution for problem `A - Journey Planning` Link: https://codeforces.com/contest/1320/problem/A Tags: data structures, dp, greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1320/submission/144987653

---
+++ codeforces/1320/A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+// use for unordered map (for high collison):
+// um.reserve(4096); um.max_load_factor(0.25);
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+ 
+const int maX = 2 * 1e5 + 1;
+ 
+ 
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<ll> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+		b[i] = i - b[i];
+	}
+	unmap<ll,ll> m;
+	m.reserve(4096); m.max_load_factor(0.25);
+	for (int i = 0; i < n; i++) {
+		m[b[i]] += (i - b[i]);
+	}
+	ll res = 0;
+	for (auto [p,q]: m) {
+		asnmx(res, q);
+	}
+	cout << res << endl;
+}
