2b966eb46e9f1a57f1ac77f45caf5d2d2065353a
Abhas Kumar <abhas113dba@gmail.com>
2022-02-03 15:42:00 +0530

    Add solution for problem `B - National Project` Link: https://codeforces.com/contest/1303/problem/B Tags: math, *1400 Ref: https://codeforces.com/contest/1303/submission/145017500

--- codeforces/1303/B.cpp
+++ codeforces/1303/B.cpp
@@ -14,25 +14,34 @@ template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;}
 template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
 template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
 template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
-typedef pair<int, int> pi;
+typedef pair<ll, ll> pi;
  
 #define PI 3.14159265
  
-const int maX = 2 * 1e5 + 1;
+const ll maX = 2 * 1e5 + 1;
+
+bool good(ll m, ll k, ll a, ll b) {
+	ll reqgood = ceil(k / 2.0);
+	return reqgood > (m / (a + b)) * a + min(m % (a + b), a);
+}
 
 int main(){
 	const ll inf = 1e18 + 7;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int tt;
+	ll tt;
 	cin >> tt;
 	while (tt--) {
-		ll n, a, b;
-		cin >> n >> a >> b;
-		ll t = n;
-		n = ceil(n / 2.0);
-		ll total = n / a * (a + b) + (n % a);
-		if (n % a == 0) total -= b;
-		cout << max(total, t) << endl;
+		ll n, g, b;
+		cin >> n >> g >> b;
+		ll l = 0, r = inf;
+		while (l + 1 < r) {
+			ll m = (l + r) / 2;
+			if (good(m, n, g, b)) {
+				l = m;
+			}
+			else r = m;
+		}
+		cout << max(r, n) << endl;
 	}
 }
