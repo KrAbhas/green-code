7a810cd1cece447ef533e79a45de149dcc3b4494
Abhas Kumar <abhas113dba@gmail.com>
2022-02-14 22:12:00 +0530

    Add solution for problem `B - Expansion coefficient of the array` Link: https://codeforces.com/contest/1159/problem/B Tags: implementation, math, *1300 Ref: https://codeforces.com/contest/1159/submission/146428728

---
+++ codeforces/1159/B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+ 
+const int maX = 1e5 + 10;
+
+int main(){
+	const ll inf = 1e18 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	ll ans = INT_MAX;
+	for (int i = 0; i < n; i++) {
+		ll x = 0, y = 0;
+		y = i;
+		x = n - 1 - i;
+		if (x == 0 && y == 0) continue;
+		ans = min(ans, a[i] / max(x, y));
+	}
+	cout << ans << endl;
+}
