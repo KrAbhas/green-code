9696f151693539a3aaffa3590e4f89231d316a6b
Abhas Kumar <abhas113dba@gmail.com>
2022-02-19 04:52:00 +0530

    Add solution for problem `C - Mortal Kombat Tower` Link: https://codeforces.com/contest/1418/problem/C Tags: dp, graphs, greedy, shortest paths, *1500 Ref: https://codeforces.com/contest/1418/submission/146881049

---
+++ codeforces/1418/C.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+ 
+const int maX = 1e5 + 10;
+
+int main(){
+	const ll inf = 1e9 + 7;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n; 
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		vector<int> his(n + 1);
+		vector<int> my(n + 1);
+		his[n] = my[n] = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			{//for his
+				his[i] = my[i + 1] + a[i];
+				if (i + 2 <= n) {
+					his[i] = min(his[i], my[i + 2] + a[i] + a[i + 1]);
+				}
+			}
+			{//for my
+				my[i] = his[i + 1];
+				if (i < n - 1) 
+					my[i] = min(his[i + 2], my[i]);
+			}
+		}
+		cout << his[0] << endl;
+	}
+}
