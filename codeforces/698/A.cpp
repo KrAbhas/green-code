591c9c18957dbb00c80e3dbac2eca69bfe16bdde
Abhas Kumar <abhas113dba@gmail.com>
2022-02-21 03:48:00 +0530

    Add solution for problem `A - Vacations` Link: https://codeforces.com/contest/698/problem/A Tags: dp, *1400 Ref: https://codeforces.com/contest/698/submission/147121289

--- codeforces/698/A.cpp
+++ codeforces/698/A.cpp
@@ -25,9 +25,13 @@ int a[101];
 int dp[101][3];
 
 void fun(int i) {
-	if (a[i] < 3) dp[i][a[i]] = 0;
-	if (a[i] == 3) dp[i][1] = 0, dp[i][2] = 0;
 	dp[i][0] = 1;
+	if (a[i] == 1 || a[i] == 0) {
+		dp[i][2] = 1;
+	}
+	if (a[i] == 2 || a[i] == 0) {
+		dp[i][1] = 1;
+	}
 }
 
 int main(){
@@ -38,18 +42,17 @@ int main(){
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 3; j++) {
-			dp[i][j] = maX;
-		}
-	}
 	fun(0);
 	for (int i = 1; i < n; i++) {
 		dp[i][0] = min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]}) + 1;
 		if (a[i] == 1 || a[i] == 3)
 			dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]);
+		else {
+			dp[i][1] = dp[i][0];
+		}
 		if (a[i] == 2 || a[i] == 3)
 			dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]);
+		else dp[i][2] = dp[i][0];
 	}
 	cout << min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]}) << endl;
 }
