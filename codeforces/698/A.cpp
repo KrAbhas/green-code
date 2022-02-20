cd0c6f1d6e1f9405dff4a89afce3f2e9f7359b6b
Abhas Kumar <abhas113dba@gmail.com>
2022-02-21 03:44:00 +0530

    Add solution for problem `A - Vacations` Link: https://codeforces.com/contest/698/problem/A Tags: dp, *1400 Ref: https://codeforces.com/contest/698/submission/147121132

--- codeforces/698/A.cpp
+++ codeforces/698/A.cpp
@@ -45,14 +45,13 @@ int main(){
 	fun(0);
 	for (int i = 1; i < n; i++) {
 		dp[i][0] = min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]}) + 1;
-		if (a[i] == 1 || a[i] == 3)
-			dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]);
-		else {
-			dp[i][1] = dp[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]);
+		if (a[i] == 2 || a[i] == 0) {
+			dp[i][1]++;
 		}
-		if (a[i] == 2 || a[i] == 3)
-			dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]);
-		else dp[i][2] = dp[i][0];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]);
+		if (a[i] == 1 || a[i] == 0)
+			dp[i][2]++;
 	}
 	cout << min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]}) << endl;
 }
