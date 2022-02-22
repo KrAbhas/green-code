c7b3aa5fff88bde72a2dcdd009165bd329fc2d64
Abhas Kumar <abhas113dba@gmail.com>
2022-02-22 07:25:00 +0530

    Add solution for problem `C - Cyclic Permutations ` Link: https://codeforces.com/contest/1391/problem/C Tags: combinatorics, dp, graphs, math, *1500 Ref: https://codeforces.com/contest/1391/submission/147225023

---
+++ codeforces/1391/C.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+const ll inf = 1e9 + 7;
+const int maX = 1e5 + 10;
+const int mn = 1010;
+ll n, k;
+ll m;
+
+template<class T1, class T2> ll power(T1 n, T2 k) {
+	ll p = n;
+	ll res = 1;
+	ll t = 1;
+	while (t <= k) {
+		if (t & k) {
+			res = (res * p) % inf;
+		}
+		p = (p * p) % inf;
+		t = t << 1;
+	}
+	return res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	ll f = 1;
+	for (int i = 1; i <= n; i++) {
+		f = (f * i) % inf;
+	}
+	cout << (f - power(2, n - 1) + inf) % inf << endl;
+}
