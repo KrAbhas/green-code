3ccf37229a0f77cfa750f85d155124d353a9c6b1
Abhas Kumar <abhas113dba@gmail.com>
2022-04-18 23:24:00 +0530

    Add solution for problem `D1 - Equalizing by Division (easy version)` Link: https://codeforces.com/contest/1213/problem/D1 Tags: brute force, implementation, *1500 Ref: https://codeforces.com/contest/1213/submission/154018183

---
+++ codeforces/1213/D1.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+template<class T> bool asnmx (T& a, const T& b){return a < b? a = b, 1: 0;} 
+template<class T> bool asnmn (T& a, const T& b){return b < a? a = b, 1: 0;}
+template<class T> bool asnminpos (T& a, const T& b, T& pos, const T& i){return b < a? a = b, pos = i, 1: 0;}
+template<class T> bool asnmaxpos (T& a, const T& b, T& pos, const T& i){return a < b? a = b, pos = i, 1: 0;}
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+const ll inf = 1e9 + 7;
+const int maxx = 2 * 1e5 + 10;
+const int mn = 1010;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(all(a));
+	vector<int> f(maxx);
+	vector<int> res(maxx);
+	for (int i = 0; i < n; i++) {
+		int t = a[i];
+		int c = 0;
+		while (t) {
+			if (f[t] < k) {
+				f[t]++;
+				res[t] += c;
+			}
+			c++;
+			t /= 2;
+		}
+	}
+	int ans = maxx;
+	for (int i = 0; i < maxx; i++) {
+		if (f[i] == k) {
+			ans = min(ans, res[i]);
+		}
+	}
+	cout << ans << endl;
+}
