7bb54460caf7bfd9e430aa935586f2a4f9f2f480
Abhas Kumar <abhas113dba@gmail.com>
2022-05-09 00:17:00 +0530

    Add solution for problem `C - Inversion Graph` Link: https://codeforces.com/contest/1638/problem/C Tags: data structures, dsu, graphs, math, *1300 Ref: https://codeforces.com/contest/1638/submission/156358289

---
+++ codeforces/1638/C.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+const ll inf = 1e9 + 7;
+const int maX = 2 * 1e5 + 10;
+const int mn = 1025;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		priority_queue<int> p;
+		ll x;
+		ll s = 0;
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> x;
+			s += x;
+			if ((i + 1) * 1LL * (i + 2) / 2 == s)
+				ans++;
+		}
+		cout << ans << endl;
+	}
+}
