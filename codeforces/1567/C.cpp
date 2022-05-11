7fbc0d8de3858d8626ab270d1594afa7c1f153cc
Abhas Kumar <abhas113dba@gmail.com>
2022-05-12 00:11:00 +0530

    Add solution for problem `C - Carrying Conundrum` Link: https://codeforces.com/contest/1567/problem/C Tags: bitmasks, combinatorics, dp, math, *1600 Ref: https://codeforces.com/contest/1567/submission/156861348

--- codeforces/1567/C.cpp
+++ codeforces/1567/C.cpp
@@ -18,16 +18,15 @@ const ll inf = 1e9 + 7;
 const int maX = 1e5 + 10;
 const int mn = 1025;
 
-ll fun (ll i, ll n, vector<ll> &a, vector<ll>&ways) {
-	if (i == n - 1) return ways[a[n - 1]];
-	else if (i == n - 2) return ways[a[n - 1]] * ways[a[n - 2]];
-	int ans = ways[a[i]] * fun(i + 1, n, a, ways);
-	if (a[i] == 0)
-		return ans;
-	a[i + 2] += 10;
-	int res = ways[a[i] - 1] * fun (i + 1, n, a, ways);
-	a[i + 2] -= 10;
-	return ans + res;
+ll ways (ll k, ll n, vector<ll> &a, vector<ll> &w) {
+	if (k == n - 1) return w[a[n - 1]];
+	else if (k == n - 2) return w[a[n - 1]] * w[a[n - 2]];
+	ll ans = w[a[k]] * ways(k + 1, n, a, w);
+	if (a[k] == 0) return ans;
+	a[k + 2] += 10;
+	ll res = w[a[k] - 1] * ways(k + 1, n, a, w);
+	a[k + 2] -= 10;
+	return ans + res; 
 }
 
 int main(){
@@ -43,17 +42,16 @@ int main(){
 			a.pb(n % 10);
 			n /= 10;
 		}
-		reverse(all(a));
-		vector<ll> ways(20);
+		reverse(a.begin(), a.end());
+		vector<ll> w(20);
 		for (int i = 0; i < 20; i++) {
 			for (int j = 0; j <= i; j++) {
 				int k = i - j;
-				if (k < 10 && j < 10) {
-					ways[i]++;
-				}
+				if (k < 10 && j < 10)
+					w[i]++;
 			}
 		}
 		n = sz(a);
-		cout << fun(0, n, a, ways) - 2 << endl;
+		cout << ways(0, n, a, w) - 2 << endl;
 	}
 }
