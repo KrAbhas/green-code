a0efe6a5ae81c5006c8d8cc23f5665af06b0b1e0
Abhas Kumar <abhas113dba@gmail.com>
2022-05-13 01:13:00 +0530

    Add solution for problem `A - Mean Inequality` Link: https://codeforces.com/contest/1526/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1526/submission/156954971

---
+++ codeforces/1526/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define intl int64_t
+#define pb(x) push_back(x)
+#define unmap unordered_map
+// use for unordered map (for high collison) with 2^x:
+// um.reserve(4096); um.max_load_factor(0.25);
+#define unset unordered_set
+#define bit_set_count(x) __builtin_popcountll(x)
+typedef pair<int, int> pi;
+ 
+#define PI 3.14159265
+const ll inf = 1e9 + 7;
+const int maxx = 1e5 + 10;
+const int mn = 1025;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(2 * n);
+		for (int i = 0; i < 2 * n; i++) {
+			cin >> a[i];
+		}
+		sort (all(a));
+		for (int i = 0; i < n; i++) {
+			cout << a[i] << ' ' << a[n + i] << ' ';
+		}
+		
+	}
+}
