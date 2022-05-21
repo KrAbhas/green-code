e4551154e02fa65a4edd1a44d74eb05a48520dc0
Abhas Kumar <abhas113dba@gmail.com>
2022-05-21 10:36:00 +0530

    Add solution for problem `B - Scenes From a Memory` Link: https://codeforces.com/contest/1562/problem/B Tags: brute force, constructive algorithms, implementation, math, number theory, *1000 Ref: https://codeforces.com/contest/1562/submission/157861929

--- codeforces/1562/B.cpp
+++ codeforces/1562/B.cpp
@@ -1,71 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-#define ll long long
-#define all(x) (x).begin(), (x).end()
-#define sz(x) (ll)(x).size()
-#define intl int64_t
-#define pb(x) push_back(x)
-#define unmap unordered_map
-// use for unordered map (for high collison) with 2^x:
-// um.reserve(4096); um.max_load_factor(0.25);
-#define unset unordered_set
-#define bit_set_count(x) __builtin_popcountll(x)
-typedef pair<ll, ll> pi;
- 
-#define PI 3.14159265
-const ll inf = 1e9 + 7;
-const ll maxx = 2 * 1e5 + 10;
-const ll mn = 1025;
-
-const int maX = 1e3 + 1;
-bool comp[maX];
-inline void sieve() {
-	memset(comp, false, sizeof(comp));
-	comp[0] = true; comp[1] = true;
-	for (int i = 2; i * i < maX; i++) {
-		if (!comp[i])
-			for (int j = 2 * i; j < maX; j += i) {
-				comp[j] = true;
-			}
-	}
+#define int long long
+int t,n;
+bool a[105];
+string s;
+inline bool prime(int p){
+    if(p==1)return 1;
+    for(register int i=2;i*i<=p;i++)if(p%i==0)return 1;
+    return 0;
 }
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int tt = 1;
-	cin >> tt;
-	sieve();
-	int arr[] = {0,1,0,0,1,0,1,0,1,1};
-	while (tt--) {
-		int n;
-		cin >> n;
-		vector<int> a(n);
-		bool ok = false;
-		vector<char> vec(n);
-		for (int i = 0; i < n; i++) cin >> vec[i];
-		for (int i = 0; i < n; i++) a[i] = vec[i] - '0';
-		for (int i = 0; i < n; i++) {
-			if (arr[a[i]]) {
-				cout << 1 << endl << a[i] << endl;
-				ok = true;
-				break;
-			}
-		}
-		if (ok)
-			continue;
-		int k;
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				k = a[i] * 10 + a[j];
-				if (comp[k]) {
-					cout <<2 << endl << k << endl, ok = true;
-					break; 
-				}
-			}
-			if (ok)
-				break;
-		}
-	}
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(0),cout.tie(0);
+    cin>>t;
+    for(register int i=1;i<=100;i++)a[i]=prime(i);
+    while(t--){
+        cin>>n>>s;
+        bool flag=1;
+        for(register int i=0;i<n&&flag;i++)if(s[i]=='1'||s[i]=='4'||s[i]=='6'||s[i]=='9'||s[i]=='8'){cout<<"1\n"<<s[i]<<endl;flag=0;}
+        if(!flag)continue;
+        for(register int i=0;i<n&&flag;i++)
+        for(register int j=i+1;j<n&&flag;j++)if(a[(s[i]-'0')*10+s[j]-'0']){
+            cout<<"2\n"<<s[i]<<s[j]<<endl;
+            flag=0;
+        }
+    }
+    return 0;
 }
